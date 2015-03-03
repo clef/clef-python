fbf3e74535e019ed622e5645483c6767a7140825
Grace Wong <grace@getclef.com>
2015-03-03 11:31:24 -0800

    updates package classifiers

--- setup.py
+++ setup.py
@@ -6,21 +6,23 @@ with open('LONG_DESCRIPTION.rst') as f:
 setup(
     name='clef',
     packages=['clef'],
-    version='0.0.3',
+    version='0.0.4',
     description='A Python wrapper for the Clef API',
     long_description=long_description,
     author='Grace Wong',
     author_email='gwongz@gmail.com',
     url='https://github.com/gwongz/clef-python',
-    download_url='https://github.com/gwongz/python-clef/tarball/0.0.3',
+    download_url='https://github.com/gwongz/python-clef/tarball/0.0.4',
     license='MIT',
     keywords=['clef', 'api'],
     classifiers=[
         'Natural Language :: English',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent,'
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
 
