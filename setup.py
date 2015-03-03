77b7b4d3b8d18c33ad5612719693aaa3f3608e80
Grace Wong <grace@getclef.com>
2015-03-03 10:54:17 -0800

    updates setup file to use new repo name

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from setuptools import setup
 
 try:
@@ -11,13 +12,13 @@ except ImportError:
 setup(
     name='clef',
     packages=['clef'],
-    version='0.0.2',
+    version='0.0.3',
     description='A Python wrapper for the Clef API',
     long_description=long_description,
     author='Grace Wong',
     author_email='gwongz@gmail.com',
-    url='https://github.com/gwongz/python-clef',
-    download_url='https://github.com/gwongz/python-clef/tarball/0.0.2',
+    url='https://github.com/gwongz/clef-python',
+    download_url='https://github.com/gwongz/python-clef/tarball/0.0.3',
     license='MIT',
     keywords=['clef', 'api'],
     classifiers=[
