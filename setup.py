bee3cfd26c76fc9df64877b214181121f8bfad80
Grace Wong <grace@getclef.com>
2015-06-19 11:27:01 -0700

    adds requests as a dependency

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ except:
 setup(
     name='clef',
     packages=['clef'],
-    version='0.0.7',
+    version='0.0.8',
     description='A Python wrapper for the Clef API',
     long_description=long_description,
     author='Grace Wong',
     author_email='gwongz@gmail.com',
     url='https://github.com/gwongz/clef-python',
-    download_url='https://github.com/gwongz/python-clef/tarball/0.0.7',
+    download_url='https://github.com/gwongz/python-clef/tarball/0.0.8',
+    install_requires=['requests>=2.7.0'],
     license='MIT',
     keywords=['clef', 'api'],
     classifiers=[
